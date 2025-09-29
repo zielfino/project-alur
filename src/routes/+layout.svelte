44e71b50224cb4a9a23ed1c1032513dc89f43d6c
Ziel TUF <zielalfinoakbar@gmail.com>
2025-09-29 14:12:26 +0700

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<section>
+	<p>test</p>
+	{@render children?.()}
+</section>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    // // 1. Import the supabase client you created earlier
+    // import { supabase } from '$lib/supabaseClient';
+
+    // 2. Create a function to handle the Google login
+    // async function handleGoogleLogin() {
+    //     const { error } = await supabase.auth.signInWithOAuth({
+    //         provider: 'google',
+    //     });
+
+    //     if (error) {
+    //         console.error('Error logging in with Google:', error);
+    //     }
+    // }
+</script>
+
+<button>
+    Login with Google
+</button>

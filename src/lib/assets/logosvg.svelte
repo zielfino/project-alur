ae8c87270024cfd7b2cdcc1b48ca99086b4260d2
Ziel TUF <zielalfinoakbar@gmail.com>
2025-10-24 15:01:18 +0700

    style: responsive footer mobile tablet laptop with reuseable agerrstle.css & mainfooter

---
+++ src/lib/assets/logosvg.svelte
@@ -0,0 +1,43 @@
+
+            <div class="w-24 pr-4 mr-4 border-e-2">
+                <svg id="Layer_2" data-name="Layer 2" class="translate-y-[2px]" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 115.23 50.43">
+                    <defs>
+                        <style>
+                        .cls-1 {
+                            fill: url(#linear-gradient);
+                        }
+
+                        .cls-1, .cls-2 {
+                            stroke-width: 0px;
+                        }
+
+                        .cls-3 {
+                            fill: #231f20;
+                            font-family: Outfit-Bold, Outfit;
+                            font-size: 36px;
+                            font-weight: 700;
+                        }
+
+                        .cls-2 {
+                            fill: url(#linear-gradient-2);
+                        }
+                        </style>
+                        <linearGradient id="linear-gradient" x1="69.84" y1="23.07" x2="97.1" y2="23.07" gradientUnits="userSpaceOnUse">
+                        <stop offset="0" stop-color="#5c6fb5"/>
+                        <stop offset=".5" stop-color="#6466af"/>
+                        <stop offset=".53" stop-color="#6a68b0"/>
+                        <stop offset=".68" stop-color="#7c70b3"/>
+                        <stop offset=".84" stop-color="#8875b5"/>
+                        <stop offset="1" stop-color="#8c77b6"/>
+                        </linearGradient>
+                        <linearGradient id="linear-gradient-2" x1="87.98" x2="115.23" xlink:href="#linear-gradient"/>
+                    </defs>
+                    <g id="Layer_1-2" data-name="Layer 1">
+                        <g>
+                        <text class="cls-3" transform="translate(0 36.14)"><tspan x="0" y="0">Alur</tspan></text>
+                        <path class="cls-1" d="m92.32,7.67c-2.09,2.86-2.85,6.48-2.07,9.94,1.22,5.4.04,11.05-3.22,15.52l-9.26,12.66c-.16.22-.42.35-.7.35h-6.36c-.71,0-1.12-.81-.7-1.38l11.48-15.69c2.09-2.86,2.85-6.49,2.07-9.95-1.22-5.39-.04-11.04,3.22-15.51l2.38-3.25c.16-.22.42-.35.7-.35h6.37c.71,0,1.12.81.7,1.38l-4.61,6.29Z"/>
+                        <path class="cls-2" d="m108.91,9.79c-2.1,2.86-2.85,6.49-2.07,9.94,1.22,5.4.04,11.05-3.22,15.52l-7.71,10.54c-.16.22-.42.35-.7.35h-6.36c-.71,0-1.12-.81-.7-1.38l9.93-13.57c2.09-2.86,2.85-6.49,2.07-9.95-1.22-5.39-.05-11.04,3.22-15.51l3.94-5.38c.16-.22.42-.35.7-.35h6.35c.71,0,1.12.81.7,1.38l-6.15,8.41Z"/>
+                        </g>
+                    </g>
+                </svg>
+            </div> 
